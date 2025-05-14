0447c9b0225a68c4adbdd01bec4480823c7f49ae
Owais-Saeed <owais.saeed@outlook.com>
2025-05-14 21:27:25 +1000

    1 UI

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <ScanForm @scanComplete="handleScanResult" />
+    <ScanResult v-if="result" :result="result" @close="result = null" />
+  </div>
+</template>
+
+<script>
+import ScanForm from './components/ScanForm.vue'
+import ScanResult from './components/ScanResult.vue'
+
+export default {
+  components: { ScanForm, ScanResult },
+  data() {
+    return {
+      result: null
+    }
+  },
+  methods: {
+    handleScanResult(resultData) {
+      this.result = resultData
+    }
+  }
+}
+</script>
